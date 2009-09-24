54ed8f9e92bd4fb0a320eb846a589bd1679bf914
Igor Filippov <igor.v.filippov@gmail.com>
2009-09-24 17:47:40 +0000

    Fixed osra.bat
    git-svn-id: svn://svn.code.sf.net/p/osra/code/trunk@446 049e15af-3938-0410-b28b-82a5db9f179f

--- osra.bat
+++ osra.bat
@@ -1,10 +1,8 @@
 @echo off
 setlocal
-set current=%CD%
-cd %~dp0%
+set exec_dir=%~dp0%
 set OMP_NUM_THREADS=1
 set PATH=%~dp0;%ProgramFiles(x86)%\gs\gs8.63\bin;%ProgramFiles(x86)%\gs\gs8.63\lib;%ProgramFiles%\gs\gs8.63\bin;%ProgramFiles%\gs\gs8.63\lib;%PATH%
 set MAGICK_CONFIGURE_PATH=%~dp0%
-osra.exe "%current%\\%*"
-cd %current%
+"%exec_dir%osra.exe" %*
 endlocal
