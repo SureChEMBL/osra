2bfaddb530a1f4354096782a006b1e54264ddf1b
Igor Filippov <igor.v.filippov@gmail.com>
2010-09-17 17:09:27 +0000

    Modified package information for linux/plugins and win32
    git-svn-id: svn://svn.code.sf.net/p/osra/code/trunk@581 049e15af-3938-0410-b28b-82a5db9f179f

--- package/win32/osra.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set exec_dir=%~dp0%
-rem set OMP_NUM_THREADS=1
-set PATH=%~dp0;%ProgramFiles(x86)%\gs\gs8.63\bin;%ProgramFiles(x86)%\gs\gs8.63\lib;%ProgramFiles%\gs\gs8.63\bin;%ProgramFiles%\gs\gs8.63\lib;%PATH%
-set MAGICK_CONFIGURE_PATH=%~dp0%
-"%exec_dir%osra.exe" %*
-endlocal
