47ebfd734f84ed3068ec3703ea5a848a8d08b69f
Igor Filippov <igor.v.filippov@gmail.com>
2011-08-18 16:17:36 +0000

    Added comments to osra_labels.h
    git-svn-id: svn://svn.code.sf.net/p/osra/code/trunk@829 049e15af-3938-0410-b28b-82a5db9f179f

--- src/osra_labels.h
+++ src/osra_labels.h
@@ -97,17 +97,146 @@ struct lbond_s
 //defines lbond_t type based on lbond_s struct
 typedef struct lbond_s lbond_t;
 
+//
+// Section: Functions
+//
+
+// Function: assemble_labels()
+//
+// assembles characters into a string for a superatom label
+//
+// Parameters:
+// letters - a vector of recognized characters
+// n_letters - the number of recognized characters
+// label - a reference to the vector which will contain superatom labels 
+//
+// Returns:
+// number of superatom labels
 int assemble_labels(vector<letters_t> &letters, int n_letters, vector<label_t> &label);
+
+// Function: find_chars()
+//
+// searches for perspective characters in the image and calls OCR routines
+//
+// Parameters:
+// p - vectorized output of Potrace routines
+// orig - original image
+// letters - vector which will contain recognized characters
+// atom - vector of atoms
+// bond - vector of bonds
+// n_atom - number of atoms
+// n_bond - number of bonds
+// height - image height
+// width - image width
+// bgColor - background color
+// THRESHOLD - black-white binarization threshold
+// max_font_width - maximum font width for the specific resolution in pixels
+// max_font_height - maximum font height for the specific resolution in pixels
+// real_font_width - detected font width
+// real_font_height - detected font height
+// verbose - flag for verbose output
+//
+// Returns:
+// number of recognized characters
 int find_chars(const potrace_path_t * p, const Image &orig, vector<letters_t> &letters, vector<atom_t> &atom, vector<
                bond_t> &bond, int n_atom, int n_bond, int height, int width, ColorGray &bgColor, double THRESHOLD,
                int max_font_width, int max_font_height, int &real_font_width, int &real_font_height, bool verbose);
+
+// Function: find_plus_minus()
+//
+// Detects plus and minus signs in atomic charge labels
+//
+// Parameters:
+// p - Potrace vectorization output
+// letters - a vector of atomic label characters
+// atom -  a vector of atoms
+// bond - a vector of bonds
+// n_atom - number of atoms
+// n_bond - number of bonds
+// height - image height
+// width - image width
+// max_font_height - maximum font height for a given resolution
+// max_font_width - maximum font width for a given resolution
+// n_letters - number of characters
+//
+// Returns:
+// new number of characters
 int find_plus_minus(const potrace_path_t *p, vector<letters_t> &letters, vector<atom_t> &atom, vector<bond_t> &bond,
                     int n_atom, int n_bond, int height, int width, int max_font_height, int max_font_width, int n_letters);
+
+// Function: clean_unrecognized_characters()
+//
+// Attempts to assemble collections of small bonds which did not pass OCR routines
+// into an unspecified atomic label "*"
+//
+// Parameters:
+// bond - vector of bonds
+// n_bond - number of bonds
+// atom - vector of atoms
+// real_font_height - font height as determined by preceeding OCR routines
+// real_font_width - font width as determined by preceeding OCR routines
+// size - minimum number of bonds to constitute a perspective character
+// letters - vector of recognized characters from OCR
+// n_letters - number of recognized characters
+//
+// Returns:
+// new value for n_letters
 int clean_unrecognized_characters(vector<bond_t> &bond, int n_bond, const vector<atom_t> &atom, int real_font_height,
                                   int real_font_width, unsigned int size, vector<letters_t> &letters, int n_letters);
+
+// Function: remove_small_terminal_bonds()
+//
+// Small terminal bonds are often a result of unrecognized atomic labels
+// This function replaces such bonds with "Xx" atomic label
+//
+// Parameters:
+// bond - vector of bonds
+// n_bond - number of bonds
+// atom - vector of atoms
+// avg - average bond length
 void remove_small_terminal_bonds(vector<bond_t> &bond, int n_bond, vector<atom_t> &atom, double avg);
+
+// Function: remove_small_bonds()
+// 
+// Removes very small single bonds or replaces them, if stand-alone and next to a character and vertical
+// with a character "l"
+//
+// Parameters:
+// bond - vector of bonds
+// n_bond - number of bonds
+// atom - vector of atoms
+// letters - vector of characters
+// n_letters  - number of characters
+// max_font_height - maximum font height
+// min_font_height - minimum font height
+// avg - average bond length
+//
+// Returns:
+// new value for n_letters
 int remove_small_bonds(vector<bond_t> &bond, int n_bond, const vector<atom_t> &atom, vector<letters_t> &letters,
                        int n_letters, int max_font_height, int min_font_height, double avg);
+
+// Function: find_fused_chars()
+//
+// Attempts to recognize characters fused to a bond
+//
+// Parameters:
+// bond - vector of bonds
+// n_bond - number of bonds
+// atom - vector of atoms
+// letters - vector of characters
+// n_letters - number of characters
+// max_font_height - maximum font height
+// max_font_width - maximum font width
+// dummy - if set, character to substitute for OCR results
+// orig  - original image
+// bgColor - background color
+// THRESHOLD - black-white threshold for image binarization
+// size - minimum number of bonds which can constitute a character
+// verbose - flag for verbose output
+//
+// Returns:
+// new value for n_letters
 int find_fused_chars(vector<bond_t> &bond, int n_bond, vector<atom_t> &atom, vector<letters_t> &letters, int n_letters,
                      int max_font_height, int max_font_width, char dummy, const Image &orig, const ColorGray &bgColor,
                      double THRESHOLD, unsigned int size, bool verbose);
