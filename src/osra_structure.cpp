084596d78a0cd55af067bc5ce62a79e1ce1ce692
Igor Filippov <igor.v.filippov@gmail.com>
2011-08-18 20:02:17 +0000

    Added comments to osra_structure.h
    git-svn-id: svn://svn.code.sf.net/p/osra/code/trunk@831 049e15af-3938-0410-b28b-82a5db9f179f

--- src/osra_structure.cpp
+++ src/osra_structure.cpp
@@ -1,3 +1,30 @@
+/******************************************************************************
+ OSRA: Optical Structure Recognition Application
+
+ This is a U.S. Government work (2007-2011) and is therefore not subject to
+ copyright. However, portions of this work were obtained from a GPL or
+ GPL-compatible source.
+ Created by Igor Filippov, 2007-2011 (igorf@helix.nih.gov)
+
+ This program is free software; you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation; either version 2 of the License, or (at your option) any later
+ version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+ St, Fifth Floor, Boston, MA 02110-1301, USA
+ *****************************************************************************/
+// File: osra_structure.cpp
+//
+// Defines main structure recognition (molecular atoms and bonds)
+// recognition routines
+//
+
 #include "osra_structure.h"
 
 void remove_disconnected_atoms(vector<atom_t> &atom, vector<bond_t> &bond, int n_atom, int n_bond)
