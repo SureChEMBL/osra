90453d7d294cf7be38f167626202ca0948990936
Dmitry Katsubo <dma_k@mail.ru>
2011-09-15 14:34:36 +0000

    Using JNATI for loading the native code.
    git-svn-id: svn://svn.code.sf.net/p/osra/code/trunk@835 049e15af-3938-0410-b28b-82a5db9f179f

--- addons/java/net/sf/osra/OsraLib.java
+++ addons/java/net/sf/osra/OsraLib.java
@@ -1,8 +1,12 @@
 package net.sf.osra;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Writer;
+import java.util.PropertyResourceBundle;
 
-import org.apache.commons.logging.LogFactory;
+import net.sf.jnati.NativeCodeException;
+import net.sf.jnati.deploy.NativeLibraryLoader;
 
 /**
  * JNI bridge for OSRA library.
@@ -12,8 +16,8 @@ import org.apache.commons.logging.LogFactory;
 public class OsraLib {
 
 	/**
-	 * Process the given image with OSRA library. For more information see the corresponding <a
-	 * href="https://sourceforge.net/apps/mediawiki/osra/index.php?title=Usage">CLI options</a>.
+	 * Process the given image with OSRA library. For more information see the corresponding <a href=
+	 * "https://sourceforge.net/apps/mediawiki/osra/index.php?title=Usage">CLI options</a>.
 	 * 
 	 * @param imageData
 	 *            the image binary data
@@ -32,31 +36,54 @@ public class OsraLib {
 	 * @return 0, if the call succeeded or negative value in case of error
 	 */
 	public static native int processImage(byte[] imageData, Writer outputStructureWriter, String format,
-				String embeddedFormat, boolean outputConfidence, boolean outputCoordinates, boolean outputAvgBondLength);
+			String embeddedFormat, boolean outputConfidence, boolean outputCoordinates, boolean outputAvgBondLength);
 
-	public static native String getVersion();
+	private static final String NAME = "osra";
+
+	private static final String VERSION;
+
+	public static String getVersion() {
+		return VERSION;
+	}
 
 	static {
 		try {
-			System.loadLibrary("osra_java");
+			VERSION = getVersionFromResource();
+
+			NativeLibraryLoader.loadLibrary(NAME, VERSION);
+		} catch (NativeCodeException e) {
+			// Unable to handle this.
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			// Unable to handle this.
+			throw new RuntimeException(e);
 		}
-		catch (UnsatisfiedLinkError e) {
-			LogFactory.getLog(OsraLib.class).error(
-						"Failed to load OSRA library from java.library.path ("
-									+ System.getProperty("java.library.path") + "): " + e.getMessage());
-			throw e;
+	}
+
+	private static final String MAVEN_PROPERTIES = "META-INF/maven/net.sf.osra/osra/pom.properties";
+
+	/**
+	 * This function looks up the OSRA version from Maven properties file.
+	 */
+	private static String getVersionFromResource() throws IOException {
+		final InputStream is = OsraLib.class.getClassLoader().getResourceAsStream(MAVEN_PROPERTIES);
+
+		try {
+			final PropertyResourceBundle resourceBundle = new PropertyResourceBundle(is);
+
+			return resourceBundle.getString("version");
+		} finally {
+			is.close();
 		}
 	}
 
 	/*
-	// Sample usage:
-	public static void main(String[] args) throws IOException {
-		final StringWriter writer = new StringWriter();
-
-		int result = processImage(IOUtils.toByteArray(new FileInputStream(args[0])), writer, "sdf", "inchi", true,
-					true, true);
-
-		System.out.println("OSRA completed with result:" + result + " structure:\n" + writer.toString() + "\n");
-	}
-	*/
+	 * // Sample usage: public static void main(String[] args) throws IOException { final StringWriter writer = new
+	 * StringWriter();
+	 * 
+	 * int result = processImage(IOUtils.toByteArray(new FileInputStream(args[0])), writer, "sdf", "inchi", true, true,
+	 * true);
+	 * 
+	 * System.out.println("OSRA completed with result:" + result + " structure:\n" + writer.toString() + "\n"); }
+	 */
 }
