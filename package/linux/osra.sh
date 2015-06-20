ae69a9d287fdca800d3e7941f0fb92a3b3d6bd67
Igor Filippov <igor.v.filippov@gmail.com>
2015-06-20 16:30:31 +0000

    Added linux package dependencies
    git-svn-id: svn://svn.code.sf.net/p/osra/code/trunk@991 049e15af-3938-0410-b28b-82a5db9f179f

--- package/linux/osra.sh
+++ package/linux/osra.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-#export OMP_NUM_THREADS=1
-GS_PATH=/usr/local/bin
-export PATH=$PATH:$GS_PATH
-export MAGICK_CONFIGURE_PATH=/opt/local/osra/1.3.5/
-/opt/local/osra/1.3.5/osra.bin $*
+export MAGICK_CONFIGURE_PATH=/opt/local/osra/2.0.0/
+/opt/local/osra/2.0.0/osra-bin $*
